e30283bc0362ba280091cf8855b01a9f20f3fda3
Danai Bavishi <danaibavishi@gmail.com>
2020-01-28 11:52:42 -0600

    Further tweeks for accuracy

--- listen_osc.py
+++ listen_osc.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
   dispatcher.map("/Sprinkle_food", print)
   dispatcher.map("/Cleaning_wave", print)
   dispatcher.map("/Finger_point", print)
-  dispatcher.map("/Easter_egg", print)
+  dispatcher.map("/WTF", print)
   #dispatcher.map("/wek/outputs", print)
   dispatcher.map("/volume", print_volume_handler, "Volume")
   dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
